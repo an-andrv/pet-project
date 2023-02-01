1df8362f04244ef02972a604074b8550a3cb6835
Anastasiia Andreeva <anastasiia.webdev@gmail.com>
2023-02-01 19:29:47 +0300

    pet: added styles, materials modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { PostsComponent } from './components/posts/posts.component';
-import { CardComponent } from './components/posts/card/card.component';
+import { CardComponent } from './components/card/card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { CardComponent } from './components/posts/card/card.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
